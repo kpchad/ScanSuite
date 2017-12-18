a2c05ed16d0c324572df9b55719a32e39be84a2d
Kyle Chadwick <kylepchad@gmail.com>
2017-12-17 18:12:08 -0800

    initial commit

---
+++ Assets/Scripts/RenderTIF.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTIF : MonoBehaviour {
+
+    public Texture2D tifFile;
+    //use TextureImporter ?
+
+	// Use this for initialization
+	void Start () {
+        int x = 1;
+        int y = 1;
+        Color color = tifFile.GetPixel(x,y);
+        print(color);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
